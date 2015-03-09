1ea5863c663f9f0ba9476e7ffd5263c5d14689e6
Daniel Koch <dkoch89@gmail.com>
2015-03-08 23:41:28 -0600

    Initial commit

---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <!-- Basic Page Needs
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta charset="utf-8">
+  <title>Your page title here :)</title>
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <!-- Mobile Specific Metas
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- FONT
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,600italic,300,600' rel='stylesheet' type='text/css'>
+
+  <!-- CSS
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/main.css">
+
+  <!-- Favicon
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="icon" type="image/png" href="images/favicon.png">
+
+</head>
+<body>
+
+  <!-- Primary Page Layout
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <div class="band header">
+    <header>
+
+        <a href="#" class="site-title">
+          <svg viewBox="0 0 360 360" xmlns="http://www.w3.org/2000/svg" width="360" height="360" version="1.1"><path d="M0 0L0 150 257.2 0 0 0zM360 0L51.4 180 360 360 360 0zM0 210L0 360 257.2 360 0 210z"/></svg>
+          <h1>Daniel P Koch</h1>
+        </a>
+
+        <a class="menu-button">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <path d="M2 6h20v3H2zm0 5h20v3H2zm0 5h20v3H2z"/>
+          </svg>
+        </a>
+
+        <nav>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Resume</a></li>
+            <li><a href="#">Projects</a></li>
+            <li><a href="#">Blog</a></li>
+          </ul>
+        </nav>
+
+      <div class="page-title">
+        <h2>Welcome</h2>
+      </div>
+
+    </header>
+  </div>
+
+  <div class="band main">
+    <main>
+      <h3>This is a heading</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a condimentum nisl. Donec pulvinar nisl libero, eget feugiat ante blandit ac. Donec volutpat vitae elit in suscipit. Integer ac ullamcorper libero. Sed imperdiet ante eget felis feugiat, ut feugiat diam hendrerit. Fusce dapibus justo ut metus facilisis tristique. Donec varius congue tortor. Mauris a sapien fermentum, tincidunt velit in, blandit lorem. Ut molestie dolor et ante pellentesque dignissim. Ut et neque quis urna maximus egestas. Donec auctor sem a magna facilisis hendrerit. Morbi placerat erat ut felis dictum, sit amet suscipit tellus ultrices.</p>
+
+      <h3>This is also a heading</h3>
+      <p>Aenean vel mi sed lacus sollicitudin maximus. Donec id lacus nibh. Sed vel leo egestas, mollis nisl sit amet, lacinia diam. Nulla facilisi. Phasellus eget ligula vulputate, bibendum dolor non, sodales erat. Mauris id purus nisl. Vivamus gravida augue id mollis gravida.</p>
+      <p>Nam vel sem mauris. Praesent sodales iaculis libero in rhoncus. Sed quis dignissim risus. Praesent eu nulla cursus, lacinia erat nec, vestibulum ex. Etiam rutrum justo ultrices pharetra malesuada. Aenean urna augue, egestas eget lorem id, vehicula pharetra elit. Ut auctor convallis justo vitae euismod. In a tincidunt metus. Donec tincidunt lacus et nisl sodales, vel aliquam felis vulputate. Ut est nulla, elementum a condimentum id, auctor eu leo.</p>
+    </main>
+  </div>
+
+  <div class="band social">
+    <footer></footer>
+  </div>
+
+  <div class="band footer">
+    <footer>
+      <a href="mailto:daniel.p.koch@gmail.com">daniel.p.koch@gmail.com</a>
+    </footer>
+  </div>
+
+<!-- End Document
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+</body>
+</html>
