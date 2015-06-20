a4560a2cbd895552be36dd89d300bcc6b49cfbd1
Daniel Koch <daniel.koch@byu.edu>
2015-06-20 09:47:09 -0500

    Made blog link in main navigation absolute

--- include/header.php
+++ include/header.php
@@ -65,7 +65,7 @@
             <li><a href="/about/">About</a></li>
             <li><a href="/resume/">Resume</a></li>
             <li><a href="/projects/">Projects</a></li>
-            <li><a href="blog" class="nav-external">Blog</a></li>
+            <li><a href="/blog" class="nav-external">Blog</a></li>
           </ul>
         </nav>
 
