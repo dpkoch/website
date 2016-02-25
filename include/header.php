7e98ac3922f6dedb998340bcea81b8aa789c87e1
Daniel Koch <dkoch89@gmail.com>
2016-02-25 00:55:17 -0700

    Replaced projects page with portfolio page

--- include/header.php
+++ include/header.php
@@ -64,7 +64,7 @@
             <li><a href="/">Home</a></li>
             <li><a href="/about/">About</a></li>
             <li><a href="/resume/">Resume</a></li>
-            <li><a href="/projects/">Projects</a></li>
+            <li><a href="/portfolio/">Portfolio</a></li>
             <li><a href="/blog" class="nav-external">Blog</a></li>
           </ul>
         </nav>
--- projects/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-  $page_title='Projects';
-  $page_heading='Projects';
-  include($_SERVER['DOCUMENT_ROOT'] . '/include/header.php');
-?>
-
-<?php include ($_SERVER['DOCUMENT_ROOT'] . '/include/footer.php'); ?>
